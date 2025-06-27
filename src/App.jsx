1abcba2e336dc38c90eb3b8f7b04c8907b7f2cb7
Sun and Boats <201173636+SunAndBoats@users.noreply.github.com>
2025-06-27 17:31:13 +1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+//named import
+import {Greeting} from './components/Greeting';
+
+//default import
+import Tucuuuulo from './components/Counter'
+function App() {
+  
+
+  return (
+    <>
+     <Greeting/>
+      <Tucuuuulo/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+export function Greeting() {
+    
+    return <p>Hello from Greeting!</p>;
+  }
+  
