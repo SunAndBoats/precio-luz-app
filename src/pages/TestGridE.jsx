f8c07c18fecb3a69a3760cf47faeb92164cc361e
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-07-26 18:19:37 +1200

    commit before mock branch

--- src/pages/TestGridE.jsx
+++ src/pages/TestGridE.jsx
@@ -3,7 +3,7 @@ import TablaVisualSimpleE from '../components/TablaVisualSimpleE';
 export default function TestGridE() {
   return (
     <div style={{ padding: '1rem' }}>
-      <h1>Tabla Visual Simple E</h1>
+      <h1>Tabla de Precios</h1>
       <TablaVisualSimpleE />
     </div>
   );
