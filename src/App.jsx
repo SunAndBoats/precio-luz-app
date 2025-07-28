41b56331078eace188ce303500d6d1d73650405c
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-07-28 14:59:40 +1200

    deleted old files and test pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,14 @@
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import styles from './styles/App.module.css';
 
-import TestData from './pages/TestData';
-import TestStatistics from './pages/TestStatistics';
-import TestBaratoCaro from './pages/TestBaratoCaro';
-import TestFecha from './pages/TestFecha';
-import TestHoraActual from './pages/TestHoraActual';
-import TestHoraNoUtc from './pages/TestHoraNoUtc';
-import TestTablaVisual from './pages/TestTablaVisual';
+
 import TestGridE from './pages/TestGridE';
 import TestAverageBox from './pages/TestAverageBox';
 import TestMinPriceBox from './pages/TestMinPriceBox';
 import TestMaxPriceBox from './pages/TestMaxPriceBox';
 import TestNowPriceBox from './pages/TestNowPriceBox';
 import TestMainPage from './pages/TestMainPage';
-import TestOnlyStats from './pages/TestOnlyStats';
-import TestOnlyTable from './pages/TestOnlyTable';
-import TestResponsiveMobile from './pages/TestResponsiveMobile';
+
 
 
 
@@ -27,15 +19,7 @@ export default function App() {
     <Router>
       <header className={styles.header}>
         <nav className={styles.nav}>
-          {/* 
-          <Link to="/TestData" className={styles.link}>Test Data</Link>
-          <Link to="/TestStatistics" className={styles.link}>Test Statistics</Link>
-          <Link to="/TestFecha" className={styles.link}>Test Fecha</Link>
-          <Link to="/TestBaratoCaro" className={styles.link}>Test Barato Caro</Link>
-          <Link to="/TestHoraActual" className={styles.link}>Hora Actual</Link>
-          <Link to="/TestHoraNoUtc" className={styles.link}>Test Hora No Utc</Link>
-          <Link to="/TestTablaVisual" className={styles.link}>Test Tabla Visual</Link>
-          */}
+          
           <Link to="/test-grid-e" className={styles.link}>Test Grid E</Link>
           <Link to="/TestAverageBox" className={styles.link}>Test Average Box</Link>
           <Link to="/TestMinPriceBox" className={styles.link}>Test Min Price Box</Link>
@@ -52,22 +36,14 @@ export default function App() {
       <main className={styles.main}>
         <Routes>
           <Route path="/" element={<Navigate to="/TestData" />} />
-          <Route path="/TestData" element={<TestData />} />
-          <Route path="/TestStatistics" element={<TestStatistics />} />
-          <Route path="/TestFecha" element={<TestFecha />} />
-          <Route path="/TestBaratoCaro" element={<TestBaratoCaro />} />
-          <Route path="/TestHoraActual" element={<TestHoraActual />} />
-          <Route path="/TestHoraNoUtc" element={<TestHoraNoUtc />} />
-          <Route path="/TestTablaVisual" element={<TestTablaVisual />} />
+          
           <Route path="/test-grid-e" element={<TestGridE />} />
           <Route path="/TestAverageBox" element={<TestAverageBox />} />
           <Route path="/TestMinPriceBox" element={<TestMinPriceBox />} />
           <Route path="/TestMaxPriceBox" element={<TestMaxPriceBox />} />
           <Route path="/TestNowPriceBox" element={<TestNowPriceBox />} />
           <Route path="/TestMainPage" element={<TestMainPage />} />
-          <Route path="/TestOnlyStats" element={<TestOnlyStats />} />
-          <Route path="/TestOnlyTable" element={<TestOnlyTable />} />
-          <Route path="/TestResponsiveMobile" element={<TestResponsiveMobile />} />
+          
 
           
 
