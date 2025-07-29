9d7886a413375298d9811d1531ff2e71d7bdb0bc
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-07-29 17:05:08 +1200

    install vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import styles from './styles/App.module.css';
-
-
 import TestGridE from './pages/TestGridE';
 import TestAverageBox from './pages/TestAverageBox';
 import TestMinPriceBox from './pages/TestMinPriceBox';
 import TestMaxPriceBox from './pages/TestMaxPriceBox';
 import TestNowPriceBox from './pages/TestNowPriceBox';
-import TestMainPage from './pages/TestMainPage';
 import TestMainPage2 from './pages/TestMainPage2';
-
-
-
+import { Analytics } from "@vercel/analytics/next"
 
 
 export default function App() {
@@ -21,52 +16,29 @@ export default function App() {
       <header className={styles.header}>
         <nav className={styles.nav}>
           
-          <Link to="/test-grid-e" className={styles.link}>Test Grid E</Link>
-          <Link to="/TestAverageBox" className={styles.link}>Test Average Box</Link>
-          <Link to="/TestMinPriceBox" className={styles.link}>Test Min Price Box</Link>
-          <Link to="/TestMaxPriceBox" className={styles.link}>Test Max Price Box</Link>
-          <Link to="/TestNowPriceBox" className={styles.link}>Precio de la luz Ahora</Link>
-          <Link to="/TestMainPage" className={styles.link}>Test Main Page</Link>
-
-          <Link to="/TestMainPage2" className={styles.link}>Test Main Page 2</Link>
-
-
-               
-
+          <Link to="/test-grid-e" className={styles.link}>Tabla</Link>
+          <Link to="/TestAverageBox" className={styles.link}>Precio Medio</Link>
+          <Link to="/TestMinPriceBox" className={styles.link}>Precio Mínimo de hoy</Link>
+          <Link to="/TestMaxPriceBox" className={styles.link}>Precio Máximo de hoy</Link>
+          <Link to="/TestNowPriceBox" className={styles.link}>Precio Hora Actual</Link>
          
         </nav>
       </header>
       <main className={styles.main}>
         <Routes>
-          <Route path="/" element={<Navigate to="/TestData" />} />
-          
+          <Route path="/" element={<TestMainPage2 />} />          
           <Route path="/test-grid-e" element={<TestGridE />} />
           <Route path="/TestAverageBox" element={<TestAverageBox />} />
           <Route path="/TestMinPriceBox" element={<TestMinPriceBox />} />
           <Route path="/TestMaxPriceBox" element={<TestMaxPriceBox />} />
           <Route path="/TestNowPriceBox" element={<TestNowPriceBox />} />
-          <Route path="/TestMainPage" element={<TestMainPage />} />
-
-          <Route path="/TestMainPage2" element={<TestMainPage2 />} />
-
-
           
 
-          
-
-          
-
-          
-          
-
-          
-
-
-          
         </Routes>
+        <Analytics/>
       </main>
       <footer className={styles.footer}>
-        <p>© {new Date().getFullYear()} Tu Web</p>
+        <p>© {new Date().getFullYear()} VAPRO SOFTWARE</p>
       </footer>
     </Router>
   );
